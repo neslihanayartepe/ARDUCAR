c324f458c0dfbd4125b7843ad99fcf9d905dcbb5
neslihanayartepe <58991602+neslihanayartepe@users.noreply.github.com>
2020-02-21 21:58:45 +0300

    Add files via upload

---
+++ sketch_sep23b.ino
@@ -0,0 +1,27 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(10, 11); // TX | RX modülün tx'i 10 nolu pin'e rx'i 11 nolu pin
+
+void setup() 
+{
+pinMode(9, OUTPUT); // bu pinmodüldeki vcc altındaki pine bağlanacak state e 
+//bağlanmayacak sadece state pin i boş bırakılacak
+digitalWrite(9, HIGH); 
+Serial.begin(9600);
+Serial.println("Enter AT commands:");
+BTSerial.begin(38400); // HC-05 default speed in AT 
+
+}
+
+void loop()
+{
+// Keep reading from HC-05 and send to Arduino 
+// Serial Monitor
+if (BTSerial.available())
+Serial.write(BTSerial.read());
+
+// Keep reading from Arduino Serial Monitor 
+// and send to HC-05
+if (Serial.available())
+BTSerial.write(Serial.read());
+}
